3f77d410eea01b0b618b89d25f06485b35408628
Dinis Costa <scorpaust@hotmail.com>
2021-05-19 15:40:06 +0100

    Handled player movement.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementSpeed = 3.0f;
+
+    private Rigidbody2D myBody;
+
+    private Vector2 moveVector;
+
+    private SpriteRenderer sr;
+
+    private float harvestTimer;
+
+    private bool isHarvesting;
+
+    private GameObject artifact;
+
+    private string MOVEMENT_AXIS_X = "Horizontal";
+
+    private string MOVEMENT_AXIS_Y = "Vertical";
+
+    private void Awake()
+	{
+        myBody = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+	private void FixedUpdate()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        MovePlayer();
+        FlipSprite();   
+    }
+
+    private void FlipSprite()
+	{
+        if (Input.GetAxisRaw(MOVEMENT_AXIS_X) == 1)
+		{
+            sr.flipX = false;
+		} 
+        else if (Input.GetAxisRaw(MOVEMENT_AXIS_X) == -1)
+		{
+            sr.flipX = true;
+		}
+	}
+
+    private void MovePlayer()
+	{
+        if (isHarvesting)
+        {
+            myBody.velocity = Vector2.zero;
+        }
+        else
+        {
+            moveVector = new Vector2(Input.GetAxis(MOVEMENT_AXIS_X), Input.GetAxis(MOVEMENT_AXIS_Y));
+
+            if (moveVector.sqrMagnitude > 1)
+            {
+                moveVector = moveVector.normalized;
+
+                myBody.velocity = new Vector2(moveVector.x * movementSpeed * Time.deltaTime, moveVector.y * movementSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
