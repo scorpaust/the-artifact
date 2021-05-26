f371cacbfe0624326c80a070147726433a9f41e3
Dinis Costa <scorpaust@hotmail.com>
2021-05-26 18:40:58 +0100

    Handled wolf AI and artifact mechanics.

---
+++ Assets/Scripts/Wolf Scripts/WolfAI.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfAI : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool isEater;
+
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    [SerializeField]
+    private int attackDamage = 5;
+
+    [SerializeField]
+    private float attackTimeThreshold = 1f;
+
+    [SerializeField]
+    private float eatTimeThreshold = 2f;
+
+    [SerializeField]
+    private LayerMask bushMask;
+
+    [HideInInspector]
+    public bool isMoving, Left;
+
+    private Artifact artifact;
+
+    private BushFruits bushFruitsTarget;
+
+    private float attackTimer;
+
+    private float eatTimer;
+
+    private bool killingBush;
+
+    private bool isAttacking;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        SearchForTarget();
+
+        if (isEater)
+		{
+            killingBush = false;
+		}
+		else
+		{
+            isAttacking = false;
+		}
+
+        artifact = GameObject.FindGameObjectWithTag("Artifact").GetComponent<Artifact>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void SearchForTarget()
+	{
+        bushFruitsTarget = null;
+
+        Collider2D[] hits;
+
+        for (int i = 0; i < 50; i++)
+		{
+            hits = Physics2D.OverlapCircleAll(transform.position, Mathf.Exp(i), bushMask);
+
+            foreach(Collider2D hit in hits)
+			{
+                if (hit && (hit.GetComponent<BushFruits>().HasFruits() && hit.GetComponent<BushFruits>().enabled))
+				{
+                    bushFruitsTarget = hit.GetComponent<BushFruits>();
+                    break;
+				}
+			}
+
+            if (bushFruitsTarget)
+                break;
+		}
+	}
+
+    private void Attack()
+	{
+        artifact.TakeDamage(attackDamage);
+	}
+}
---
+++ Assets/Scripts/Wolf Scripts/WolfAnim.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfAnim : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] wolfAnimSprites;
+
+	[SerializeField]
+	private float animTimeThreshold = 0.15f;
+
+	private SpriteRenderer sr;
+
+	private WolfAI wolfAI;
+
+    private int state = 0;
+
+    private float animTimer;
+
+	private void Awake()
+	{
+        sr = GetComponent<SpriteRenderer>();
+		wolfAI = GetComponent<WolfAI>();
+	}
+
+	private void Update()
+	{
+		if (wolfAI.isMoving)
+		{
+			if (Time.time > animTimer)
+			{
+				sr.sprite = wolfAnimSprites[state % wolfAnimSprites.Length];
+
+				state++;
+
+				if (state == wolfAnimSprites.Length)
+					state = 0;
+
+				animTimer = Time.time + animTimeThreshold;
+			}
+		}
+		else
+		{
+			sr.sprite = wolfAnimSprites[0];
+		}
+		sr.flipX = wolfAI.Left;
+	}
+}
+
