e3c7ca21076395c718d228c73e2b17aaf05e80df
Dinis Costa <scorpaust@hotmail.com>
2021-05-28 23:37:45 +0100

    Main Menu Screen and Game Over Screen.

---
+++ Assets/Scripts/System Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField]
+    private Canvas howToPlay;
+
+    public void PlayGame()
+	{
+        SceneManager.LoadScene("Gameplay");
+	}
+
+    public void HowToPlay()
+	{
+        howToPlay.enabled = true;
+	}
+
+    public void BackToMainMenu()
+	{
+        howToPlay.enabled = false;
+	}
+}
